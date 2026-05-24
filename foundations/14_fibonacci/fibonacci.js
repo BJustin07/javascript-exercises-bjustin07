7f3ab0860237bbd3ed00cee99b568791b0b6733b
bjustin07 <baquiranjustin07@gmail.com>
2026-05-24 18:37:06 +0800

    foundations:14_fibonacci.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    const initFib = [0,1]
+    if(Number(n) === 0){
+        return 0
+    }
+    if(Number(n)=== 1){
+        return 1
+    }
+    if(n < 0){
+        return "OOPS"
+    }
+    for(let i = 2; i < Number(n); i++){
+        initFib[i] = initFib[i - 1] + initFib[i-2]
+    }
+    return initFib[initFib.length - 1] + initFib[initFib.length -2]
 };
 
 // Do not edit below this line

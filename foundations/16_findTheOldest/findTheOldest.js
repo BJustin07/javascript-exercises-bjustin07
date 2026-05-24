5b9306de1882ed5238b184d3e1b3f117f221a574
bjustin07 <baquiranjustin07@gmail.com>
2026-05-24 18:55:50 +0800

    foundations:16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArr) {
+    const currentYear = new Date().getFullYear();
+    const sortedPeople = peopleArr
+        .map(person => {
+            return {
+                name: person.name,
+                age: person.yearOfDeath === undefined ? 
+                (currentYear - person.yearOfBirth) : (person.yearOfDeath - person.yearOfBirth)
+            }
+        })
+        .sort((a,b) => b.age - a.age)
+    return sortedPeople[0]
 
 };
 

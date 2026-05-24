62db5b29ba4f03038d1cf2dfe0639c04b9af66c4
bjustin07 <baquiranjustin07@gmail.com>
2026-05-24 18:12:12 +0800

    foundations: 13_palindromes.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const allowed = "abcdefghijklmnopqrstuvwzyx1234567890";
+    let reversed = "";
+    const stringSplit = str
+        .toLowerCase()
+        .split("")
+        .filter(str => allowed.includes(str))
+        .reverse()
+        .join("")
+    // console.log(stringSplit)
+    const formattedString = str
+        .toLowerCase()
+        .split("")
+        .filter(str => allowed.includes(str))
+        .join("")
+    // console.log(formattedString)
+    if(stringSplit === formattedString){
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
